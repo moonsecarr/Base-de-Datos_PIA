140572982c71b6bea0cf89ec903a50fddf4a636a
moonsecarr <monse.carranza@outlook.com>
2025-12-02 19:49:49 -0600

    Barra Nav a MainADMIN

--- Footballers/views/administrador/mainAdmin.view.php
+++ Footballers/views/administrador/mainAdmin.view.php
@@ -10,14 +10,22 @@
     <link rel="stylesheet" href="/assets/css/styles.css" />
 </head>
 <header class="main_header" style="position: static;">
-    <div class="row" id="componentes_header">
+    <div class="row align-items-center" id="componentes_header">
         <div class="col align-items-center">
             <a href="/administrador/main" class="iconos_link me-3" title="Página Principal">
                 <img src="/assets/image/footballers.png" class="float-start imagen_logo" alt="nameapp">
             </a>
         </div>
 
-        <div class="col iconos d-flex justify-content-end align-items-center">
+        <div class="col d-flex nav_bar align-items-center justify-content-center" >
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2 barraNav" type="search" placeholder="Search" aria-label="Search"/>
+                    <button class="btn btn-outline-success btn_Nav" type="submit"><img class="iconos_img" src="/assets/iconos/Search.png"></button>
+                </form>
+
+            </div>
+
+        <div class="col iconos d-flex justify-content-end">
             <a href="/crearMundial" class="iconos_link me-3" title="Crear mundiales">
                 <img src="/assets/iconos/earth.png" class="float-end iconos_img" alt="icono de likes">
             </a>
